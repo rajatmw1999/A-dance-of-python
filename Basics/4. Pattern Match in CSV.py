ea85a6ea7fc066d746a8b1a017edc0cdd7978478
Rajat Upadhyay <rupadhyay@automotivemastermind.com>
2021-06-29 17:25:33 +0530

    csv patterns more

--- Basics/4. Pattern Match in CSV.py
+++ Basics/4. Pattern Match in CSV.py
@@ -1,10 +1,13 @@
 # find if a column has only numerical values
 # find if a column contains only limited values, M or F in gender
 # find if a column is totally empty
-# every row contains a particular character
 # every row starts with the same character
 # every row ends with the same character
 # check if some rows are null and others are not
+
+# TO-DO:
+# every row contains a particular character
+# check for patterns in start of value with length more than 1, like bryan and brian start with br and not just b
 
 def only_numeric(list):
     ans = -1
@@ -47,6 +50,49 @@ def all_null(list):
             break
     if ans!=-1:
         print(ans)
+
+def same_start(list):
+    ans = -1
+    temp = list[0][0]
+    for x in list:
+        if temp != x[0]:
+            ans = -1
+            break
+        else:
+            ans = "EVERY VALUE STARTS WITH '{}'".format(temp)
+    if ans!=-1:
+        print(ans)
+
+def same_end(list):
+    ans = -1
+    temp = list[0][len(list[0])-1]
+    for x in list:
+        if temp != x[len(x)-1]:
+            ans = -1
+            break
+        else:
+            ans = "EVERY VALUE ENDS WITH '{}'".format(temp)
+    if ans!=-1:
+        print(ans)
+
+def only_some_nulls(list):
+    cntnull=0
+    cntnotnull=0
+    ans = -1
+    for x in list:
+        if x is None:
+            cntnull = cntnotnull + 1
+        else:
+            cntnotnull = cntnotnull + 1
+    if cntnotnull and cntnull:
+        ans = "BOTH NULL AND NON-NULL VALUES PRESENT"
+    if ans!=-1:
+        print(ans)
+
+
 # only_numeric([23,34, "34"])
 # limited_values([23,34,23,56,35,111,23,111,56,56,56,56,23,23,23,23,111])
-all_null([None,None,None])
+# all_null([None,None,None])
+# same_start(["bryan","brjay","brman","brrun"])
+# same_end(["bryan","brjan","brman","brrun"])
+# only_some_nulls(["wegwg",None,23])
